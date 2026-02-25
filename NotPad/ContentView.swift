8a9dfb2dd04e47a88c2e3e4dc47508ff08782413
Dev-MuTTiNeeR <cemakkaya1312@gmail.com>
2026-02-25 10:46:34 -0300

    Initial Commit

---
+++ NotPad/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NotPad
+//
+//  Created by Cem Akkaya on 25/02/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NotPad/NotPadApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotPadApp.swift
+//  NotPad
+//
+//  Created by Cem Akkaya on 25/02/26.
+//
+
+import SwiftUI
+
+@main
+struct NotPadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
